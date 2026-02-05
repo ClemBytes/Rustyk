adf1dc7d60b4b516db9ab0cf8e1d3c252d212b61
Clémentine Scohy <clembytes@proton.me>
2026-02-05 21:49:56 +0100

    Minifb test

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,53 @@
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, ScaleMode, Window, WindowOptions};
 
-const WIDTH: usize = 640;
-const HEIGHT: usize = 360;
+const WIDTH: usize = 1280;
+const HEIGHT: usize = 720;
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut noise;
+    let mut carry;
+    let mut seed = 0xbeefu32;
+
+    let mut buffer = vec![0u32; WIDTH * HEIGHT];
 
     let mut window = Window::new(
-        "Test - ESC to exit",
+        "Noise Test - Press ESC to exit",
         WIDTH,
         HEIGHT,
-        WindowOptions::default(),
+        WindowOptions {
+            resize: true,
+            scale_mode: ScaleMode::UpperLeft,
+            ..WindowOptions::default()
+        },
     )
-    .unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
+    .expect("Unable to create the window");
 
-    // Limit to max ~60 fps update rate
     window.set_target_fps(60);
 
+    let mut size = (0, 0);
+
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            *i = 0; // write something more funny here!
+        let new_size = window.get_size();
+        if new_size != size {
+            size = new_size;
+            buffer.resize(size.0 * size.1, 0);
         }
 
-        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+        for pixel in buffer.iter_mut() {
+            noise = seed;
+            noise >>= 3;
+            noise ^= seed;
+            carry = noise & 1;
+            noise >>= 1;
+            seed >>= 1;
+            seed |= carry << 30;
+            noise &= 0xFF;
+
+            *pixel = (noise << 16) | (noise << 8) | noise;
+        }
+
+        window
+            .update_with_buffer(&buffer, new_size.0, new_size.1)
+            .unwrap();
     }
 }
